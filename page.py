4a8cd43897c57c8e64f7642cbabe735d0a660583
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2023-04-06 12:52:44 -0800

    Add files via upload

--- page.py
+++ page.py
@@ -1,59 +1,38 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from random import randrange
 from typing import List
 
+# Diaz Chavelas Jafet Misael 
 
 class Subject(ABC):
-    """
-    The Subject interface declares a set of methods for managing subscribers.
-    """
 
     @abstractmethod
     def registerObserver(self, observer: Observer) -> None:
-        """
-        Attach an observer to the subject.
-        """
         pass
 
     @abstractmethod
     def removeObserver(self, observer: Observer) -> None:
-        """
-        Detach an observer from the subject.
-        """
         pass
 
     @abstractmethod
     def notifyObserver(self) -> None:
-        """
-        Notify all observers about an event.
-        """
         pass
+
+# Diaz Chavelas Jafet Misael 
 
 class DispleyElement(ABC):
     @abstractmethod
     def display(self):
         pass
 
-class whaterdata(Subject):
-    """
-    The Subject owns some important state and notifies observers when the state
-    changes.
-    """
+# Diaz Chavelas Jafet Misael 
 
+class whaterdata(Subject):
     temperatura: int =None
     humidity: int =None
     pressure: int =None  
-    """
-    For the sake of simplicity, the Subject's state, essential to all
-    subscribers, is stored in this variable.
-    """
-
     _observers: List[Observer] = []
-    """
-    List of subscribers. In real life, the list of subscribers can be stored
-    more comprehensively (categorized by event type, etc.).
-    """
+ 
 
     def registerObserver(self, observer: Observer) -> None:
         print("Subject: Attached an observer.")
@@ -62,68 +41,53 @@ class whaterdata(Subject):
     def removeObserver(self, observer: Observer) -> None:
         self._observers.remove(observer)
 
-    """
-    The subscription management methods.
-    """
+
 
     def notifyObserver(self) -> None:
-        """
-        Trigger an update in each subscriber.
-        """
+
 
         print("Subject: Notifying observers...")
         for observer in self._observers:
             observer.update(self)
 
     def measuremetsChanged(self,temperatura,humidity,pressure) -> None:
-        """
-        Usually, the subscription logic is only a fraction of what a Subject can
-        really do. Subjects commonly hold some important business logic, that
-        triggers a notification method whenever something important is about to
-        happen (or after it).
-        """
 
         print("\nSubject: I'm doing something important.")
-        self.temperatura = 10
-        self.humidity = 10
-        self.pressure = 10
+        self.temperatura = temperatura
+        self.humidity = humidity
+        self.pressure = pressure
 
         self.notifyObserver()
 
+# Diaz Chavelas Jafet Misael 
 
 class Observer(ABC):
-    """
-    The Observer interface declares the update method, used by subjects.
-    """
+
+
 
     @abstractmethod
     def update(self, subject: Subject) -> None:
-        """
-        Receive update from subject.
-        """
         pass
 
-
-"""
-Concrete Observers react to the updates issued by the Subject they had been
-attached to.
-"""
+# Diaz Chavelas Jafet Misael 
 
 class CurrenConditionsDisplay(Observer): 
     def update(self, subject: Subject):
-        print(f"Current cdonditions {subject.temperatura}  F degrees and")
+        T= subject.temperatura
+        RH= subject.humidity
+        a = 16.923 + 1.85212 * 10**-1 * T + 5.37941 * RH - 1.00254 * 10**-1 *T * RH + 9.41695 * 10**-3 * T**2 + 7.28898 * 10**-3 * RH**2 + 3.45372 *10**-4 * T**2 * RH - 8.14971 * 10**-4 * T * RH**2 + 1.02102 * 10**-5 * T**2 *RH**2 - 3.8646 * 10**-5 * T**3 + 2.91583 * 10**-5 * RH**3 + 1.42721 * 10**-6* T**3 * RH + 1.97483 * 10**-7 * T * RH**3 - 2.18429 * 10**-8 * T**3 * RH**2+ 8.43296 * 10**-10 * T**2 * RH**3 - 4.81975 * 10**-11 * T**3 * RH**3
+        print(f"Current cdonditions {subject.temperatura}  F degrees and {subject.humidity} % humidity")
+        print(f"Heat Index is {a}")
 
 
+# Diaz Chavelas Jafet Misael 
 
 if __name__ == "__main__":
-    # The client code.
-
+  
     subject = whaterdata()
-
     observer_a = CurrenConditionsDisplay()
     subject.registerObserver(observer_a)
-
- 
-
-    subject.measuremetsChanged(10,10,10)
+    subject.measuremetsChanged(80,65,30.4)
+    subject.measuremetsChanged(82,70,29.2)
+    subject.measuremetsChanged(78,90,29.2)
     subject.removeObserver(observer_a)
