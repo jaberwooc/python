c1b358ce98c94baa6efd61f2281a64386d8c4581
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2023-04-15 07:15:00 -0700

    Update Factory_method.py

--- Factory_method.py
+++ Factory_method.py
@@ -1,5 +1,3 @@
-
-
 from __future__ import annotations
 from abc import ABC, abstractmethod
 import os
@@ -9,72 +7,120 @@ import os
 
 class PizzaStore(ABC):
 
-    def create_pizza(self, type):
-      pass
+    def create_pizza(type):
+        
+        pass
+    
+    def order_pizza(pizza: PizzaStore,type) -> None:
+        product = pizza.create_pizza(type)
+        product.cut()
+  
+
+        
 class NyPizzaStore(PizzaStore):
     
-    def create_pizza(self, type):
+    def create_pizza(type):
         os.system("cls")
-        if type == '1':
-            return ChessePizza()
-        elif type == '2':
-            return VeggiePizza()
-        elif type == '3':
-            return ClamPizza()
-        elif type == '4':
-            return PepperoniPizza()
+        if type == 'ChessePizza':
+            return NYStyleChessePizza()
+        elif type == 'VeggiePizza':
+            return NYStyleVeggiePizza()
+        elif type == 'ClamPizza':
+            return NYStyleClamPizza()
+        elif type == 'PepperoniePizza':
+            return NYStylePepperoniPizza()
         else:
             return print('Selecciona una opcion valida ')
         
 class ChicagoPizzaStore(PizzaStore):
-    def create_pizza(self, type):
-        return super().create_pizza(type)
-    
+    def create_pizza(type) -> Pizza:
+        os.system("cls")
+        if type == 'ChessePizza':
+            return ChicagoStyleChessePizza()
+        elif type == 'VeggiePizza':
+            return ChicagoStyleVeggiePizza()
+        elif type == 'ClamPizza':
+            return ChicagoStyleClamPizza()
+        elif type == 'PepperoniePizza':
+            return ChicagoStylePepperoniPizza()
+        else:
+            return print('Selecciona una opcion valida ')
+        
 # Productos
 class Pizza(ABC):
-    @abstractmethod
-    def prepare(self) -> str:
-        pass
-
-    def bake(self) -> str:
+    toppings = []
+    def prepare(type) -> str:
+        print(f"prepparing of the {type}")
+        print("Tossing Dough")
+        print("Adding Sauce")
+        print("Adding Toppings")
+        
+    def bake() -> str:
         return print("Bake for 25 minutes at 350")
-
-    def cut(self) -> str:
+    @abstractmethod
+    def cut() -> str:
         return print("Cutting the pizza into diagonal alices")
 
-    def box(self) -> str:
+    def box() -> str:
         return print("Place pizza in official PizzaStore box")
 
 
-# Productos concretos
-class NYChessePizza(Pizza):
-    def prepare(self) -> str:
-        return print("{prepparing of the ChessePizza}")
+# Productos concretos ny
+class NYStyleChessePizza(Pizza):
+    def __init__(self) -> None:
+        namme = 'NY style Sauce and Chesse Pizza'
+        dough = 'Thin Crust dough'
+        sauce = 'Marinara Sauce'
+  
+    def cut() -> str:
+        return print("Cutting the pizza into square alices")
 
 
-class VeggiePizza(Pizza):
+class NYStyleVeggiePizza(Pizza):
     def prepare(self) -> str:
         return print("{prepparing of the VeggiePizza}")
 
 
-class ClamPizza(Pizza):
+class NYStyleClamPizza(Pizza):
     def prepare(self) -> str:
-        return print("{Resprepparingult of the ClamPizza}")
+        return print("{prepparing  of the ClamPizza}")
 
 
-class PepperoniPizza(Pizza):
+class NYStylePepperoniPizza(Pizza):
     def prepare(self) -> str:
         return print("{prepparing of the PepperoniPizza}")
 
 
-def order_pizza(pizza: Simplepizzafactory, type) -> None:
-    product = pizza.create_pizza(type)
-    product.prepare()
-    product.bake()
-    product.cut()
-    product.box()
+# Productos concretos chicago
+class ChicagoStyleChessePizza(Pizza):
+    def __init__(self) -> None:
+        self.namme = 'Chicago style Sauce and Chesse Pizza'
+        dough = 'Thin Crust dough'
+        sauce = 'Marinara Sauce'
+    def cut() -> str:
+        return print("Cutting the pizza into square alices")
+
+        
 
 
+class ChicagoStyleVeggiePizza(Pizza):
+    def prepare(self) -> str:
+        return print("{prepparing of the VeggiePizza}")
+
+
+class ChicagoStyleClamPizza(Pizza):
+    def prepare(self) -> str:
+        return print("{prepparing of the ClamPizza}")
+
+
+class ChicagoStylePepperoniPizza(Pizza):
+    def prepare(self) -> str:
+        return print("{prepparing of the PepperoniPizza}")
+
+
+
+
+        
 if __name__ == "__main__":
     print("Hola que vas a Ordenar?")
     print("Selecciona 1 para ChessePizza")
@@ -82,4 +128,12 @@ if __name__ == "__main__":
     print("Selecciona 3 para ClamPizza")
     print("Selecciona 4 para PepperoniePizza")
     type = input('opcion :')
-    order_pizza(Simplepizzafactory(), type)
+
+    if type == '1':
+        PizzaStore.order_pizza(NyPizzaStore,'ChessePizza')
+    elif type == '2':
+        PizzaStore.order_pizza(NyPizzaStore,'VeggiePizza')
+    if type == '3':
+        PizzaStore.order_pizza(NyPizzaStore,'ClamPizza')
+    elif type == '4':
+        PizzaStore.order_pizza(NyPizzaStore,'PepperoniePizza')
