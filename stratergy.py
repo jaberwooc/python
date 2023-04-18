cd1b4605f219d9e4f8d8cb41ac92963043de9a40
Jafet Diaz <79779934+jaberwooc@users.noreply.github.com>
2023-04-17 17:55:43 -0700

    Update stratergy.py

--- stratergy.py
+++ stratergy.py
@@ -54,6 +54,9 @@ class DecoyDuck(Duck):
         self.quackbehaivor = MuteQuack()
     def display(self):
         return print('Im model Duck DecoyDuck')
+    
+    def swim(self):
+        return print('I cant float!')
 
 class FlyBehaivor(ABC):
     @abstractmethod
@@ -94,8 +97,8 @@ class MuteQuack(Quack):
 
 if __name__ == "__main__" :
     salir = True
+    type = 0
     while(salir):
-        os.system('cls')
         print('MENU')
         print('select the option')
         print('Press 1 - Select Duck')
@@ -104,6 +107,8 @@ if __name__ == "__main__" :
         print('Press 4 - Change Quack')
         print('Press 5 - Exit Program')
         op = input()
+        os.system('cls')
+
         if op =='1':
             print('Ducks!')
             print('select the option')
@@ -112,6 +117,8 @@ if __name__ == "__main__" :
             print('Select 3 for RubberDuck ')
             print('Select 4 for DecoyDuck ')
             type = input('option : ')
+            os.system('cls')
+
             if type == '1':
                 mallar = MallardDuck()
             elif type == '2':
@@ -122,24 +129,56 @@ if __name__ == "__main__" :
                 mallar = DecoyDuck()
            
         elif op =='2':
-            if type == '1':
-                mallar.perfomfly()
-                mallar.setflybehaivor(FlyRocketPowered())
-                mallar.perfomfly()
-                mallar.perfomQuack()
-            elif type == '2':
-                mallar.perfomfly()
-                mallar.setflybehaivor(FlyRocketPowered())
-                mallar.perfomfly()
-                mallar.perfomQuack()
-            elif type == '3':
-                mallar.perfomfly()
-                mallar.setflybehaivor(FlyRocketPowered())
-                mallar.perfomfly()
-                mallar.perfomQuack()
-            elif type == '4':
-                mallar.perfomfly()
-                mallar.setflybehaivor(FlyRocketPowered())
-                mallar.perfomfly()
-                mallar.perfomQuack()
-        
+                if type != 0:
+                    mallar.perfomfly()
+                    mallar.perfomQuack()
+                    mallar.swim()
+                else:
+                    print('First selecte Duck!!')
+          
+        elif op =='3':
+            if type != 0:
+                    print('CHANGE')
+                    print('select the option')
+                    print('Press 1 - FlyRocketPowered ')
+                    print('Press 2 - FlyWithWing')
+                    print('Press 3 - FlyNoWay')
+                    cf = input('option:')
+                    os.system('cls')
+
+                    if cf == '1':
+                        mallar.setflybehaivor(FlyRocketPowered())
+                    elif cf == '2':
+                        mallar.setflybehaivor(FlyWithWing())
+                    elif cf == '3':
+                        mallar.setflybehaivor(FlyNoWay())
+                    else :
+                        print('Select Valid Option')
+            else:
+                    print('First selecte Duck')
+
+        elif op =='4':
+            if type != 0:
+                    print('CHANGE')
+                    print('select the option')
+                    print('Press 1 - Quack ')
+                    print('Press 2 - Squeak')
+                    print('Press 3 - MuteQuack')
+                    cq = input('option:')
+                    os.system('cls')
+
+                    if cq =='1':
+                        mallar.setQuackbehaivor(Quack())
+                    elif cq =='2':
+                        mallar.setQuackbehaivor(Squeak())
+                    elif cq =='3':
+                        mallar.setQuackbehaivor(MuteQuack())
+                    else :
+                        print('Select Valid Option')
+
+            else:
+                    print('First selecte Duck')
+        elif op =='5':
+            salir = False
+        else:
+            print('Select Valid Option')
